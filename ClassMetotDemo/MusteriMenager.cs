4e5af724d13db634153b083e17dcf21254d42996
AysegulOfluoglu <77414234+AysegulOfluoglu@users.noreply.github.com>
2021-01-25 23:00:34 +0300

    Add files via upload

---
+++ ClassMetotDemo/MusteriMenager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriMenager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("musteri basariyla eklendi: " + musteri.Ad + " " + musteri.Soyad);
+            Console.WriteLine("*************************");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("*****************************");
+                Console.WriteLine("Musteri Ad Soyad: " + musteri.Ad + " " + musteri.Soyad);
+                Console.WriteLine("Musteri Id:" + musteri.Id);
+                Console.WriteLine("Musteri Yas: " + musteri.Yas);
+            }
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("*******************************");
+            Console.WriteLine("Musteri basariyla silindi: " + musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
