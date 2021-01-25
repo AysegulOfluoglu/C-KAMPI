be9fa09fba1c843b894fa35b56a84918bc6d24e2
AysegulOfluoglu <77414234+AysegulOfluoglu@users.noreply.github.com>
2021-01-25 23:07:55 +0300

    Add files via upload

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type Safety = Tip Güvenliği
+            //Do Not Repeat Yourself = Kendini Tekrarlama
+            // Alias = Değer Tutucu
+
+            String kategoriEtiketi = "Kategoriler";
+            bool sistemeGirisYapmismi = true;
+            double dolarDun = 7.20;
+            double dolarBugun = 7.75;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+
+            if (sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+            Console.ReadLine();
+        }
+    }
+}
