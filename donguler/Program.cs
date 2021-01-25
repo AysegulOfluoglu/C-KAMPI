550af210052415c75d79091df93e50b51d4726a5
AysegulOfluoglu <77414234+AysegulOfluoglu@users.noreply.github.com>
2021-01-25 23:51:31 +0300

    Add files via upload

---
+++ donguler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array = Dizi
+            //string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            //string kurs2 = "Programlamaya BAşlangıç İçin Temel Kurs";
+            //string kurs3 = "Java";
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı" ,
+                "Programlamaya BAşlangıç İçin Temel Kurs" ,
+                "Java" , "Python" , "C#"};
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+        }
+    }
+}
