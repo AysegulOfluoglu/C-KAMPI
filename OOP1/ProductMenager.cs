366e5742a94b41a4480b5083be2f1c38952375dc
AysegulOfluoglu <77414234+AysegulOfluoglu@users.noreply.github.com>
2021-01-26 21:04:09 +0300

    Add files via upload

---
+++ OOP1/ProductMenager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductMenager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");
+        }
+  //Void = Emir kipiyle işleme geçmesi (git ekle, git sil vs.)
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "güncellendi.");
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";  //Ürün İsmi
+            product1.UnitPrice = 500;       //Stok Fiyatı
+            product1.UnitInStock = 3;       //Stok Adedi
+
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitInStock = 5,
+                ProductName = "Kalem",
+                UnitPrice = 35,
+            };
+
+   //PascalCase = İlk harf büyük    //camelCase = İlk harf küçük sonraki kelime büyük 
+            ProductMenager productMenager = new ProductMenager();
+            productMenager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+        }
+    }
+}
